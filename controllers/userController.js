8d4efbd64d0584688f3a2f5c5b07ee6024cf3488
stuti-project <shahstuti1712@gmail.com>
2025-03-05 23:43:47 +0530

    tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ const userRegistration = async (req, res) => {
     try {
         const { FullName, UserName, Email, Password, ConfirmPassword, Country, State, EducationLevel, Subject, StudyGoals } = req.body;
         
-        // Check if user already exists
         const existingUser = await User.findOne({ Email });
         if (existingUser) {
             return res.status(400).json({ message: "Email already in use" });
@@ -16,28 +15,16 @@ const userRegistration = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(Password, 10);
 
-        // Check if a file was uploaded
         const ProfilePicture = req.file ? `uploads/images/${req.file.filename}` : null;
 
         const newUser = new User({
-            ProfilePicture,
-            FullName,
-            UserName,
-            Email,
-            Password: hashedPassword,
-            ConfirmPassword,
-            Country,
-            State,
-            EducationLevel,
-            Subject,
-            StudyGoals
+            ProfilePicture,FullName,UserName,Email,Password: hashedPassword,ConfirmPassword,Country,State,EducationLevel,Subject,StudyGoals
         });
 
         await newUser.save();
 
-        // Generate JWT Token
         const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-        res.cookie("token", token, { httpOnly: true }); // Secure cookie
+        res.cookie("token", token, { httpOnly: true }); 
 
         return res.status(200).json({ message: "Success", user: newUser });
 
@@ -56,14 +43,13 @@ const userLogin = async (req, res) => {
         }
         
         const pwdMatch = await bcrypt.compare(Password, userFound.Password);
-      
         if (!pwdMatch) {
             return res.status(400).json({ "mesage": "Invalid email or pwd" });
         }
 
         const token = jwt.sign({ _id: userFound._id  }, process.env.jwt_secret, { expiresIn: "1d" });
-
-        res.cookie("token", token, { httpOnly: true }); // Secure cookie
+        res.cookie("token", token, { httpOnly: true }); 
+        
         return res.status(200).json({ "messgae": "login success" });
 
     } catch (error) {
